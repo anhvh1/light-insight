4d82bbe2130bd6ee1cfb5a6b86d50cae9e197cb8
PhiNV <phidzai007@gmail.com>
2026-03-10 08:56:07 +0700

    Dựng layout

---
+++ LightInsight/Dashboard/Dashboard/DashboardViewItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoOS.Platform.Client;
+using VideoOS.Platform.UI.Controls;
+
+namespace LightInsight.Dashboard.Dashboard
+{
+    public class DashboardViewItem : ViewItemPlugin
+    {
+        private DashboardViewItemManager _manager;
+        internal protected static VideoOSIconSourceBase _treeNodeImage;
+        public DashboardViewItem()
+        {
+            _treeNodeImage = new VideoOSIconUriSource
+            {
+                Uri = new Uri("pack://application:,,,/SCWorkSpace;component/Resources/WorkSpace.png")
+            };
+        }
+        public override Guid Id => new Guid("2FB59DE8-A742-45C8-9EE5-9A8D9CBC3EE5");
+
+        public override string Name => "WorkSpace Plugin View Item";
+        public override VideoOSIconSourceBase IconSource => _treeNodeImage;
+
+        public override void Close()
+        {
+            
+        }
+        public override ViewItemManager GenerateViewItemManager()
+        {
+            _manager = new DashboardViewItemManager();
+            return _manager;
+        }
+      
+
+        public override void Init()
+        {
+            GenerateViewItemManager();
+        }
+    }
+}
--- LightInsight/LightInsightDefinition.cs
+++ LightInsight/LightInsightDefinition.cs
@@ -1,3 +1,4 @@
+using LightInsight.Dashboard.Dashboard;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -24,10 +25,37 @@ namespace LightInsight
     /// </summary>
     public class LightInsightDefinition : PluginDefinition
     {
-        public override Guid Id => throw new NotImplementedException();
+        private List<WorkSpacePlugin> _workSpacePlugins = new List<WorkSpacePlugin>();
+        private List<ViewItemPlugin> _workSpaceViewItemPlugins = new List<ViewItemPlugin>();
+        public override Guid Id => new Guid("55A448B4-4487-4BEF-9DBC-892C14F7D3C0");
 
-        public override string Name => throw new NotImplementedException();
+        public override string Name => "Light Insight";
 
         public override Image Icon => throw new NotImplementedException();
+
+        public override void Init()
+        {
+           
+            if (EnvironmentManager.Instance.EnvironmentType == EnvironmentType.SmartClient)
+            {
+                _workSpacePlugins.Add(new LightInsightWorkSpacePlugin());
+                _workSpaceViewItemPlugins.Add(new DashboardViewItem());
+
+            }
+        }
+        public override void Close()
+        {
+            _workSpacePlugins.Clear();
+            _workSpaceViewItemPlugins.Clear();
+        }
+        // Removed the override keyword as PluginDefinition does not define ViewItemManagers as abstract or virtual.
+        public override List<WorkSpacePlugin> WorkSpacePlugins
+        {
+            get { return _workSpacePlugins; }
+        }
+        public override List<ViewItemPlugin> ViewItemPlugins
+        {
+            get { return _workSpaceViewItemPlugins; }
+        }
     }
 }
