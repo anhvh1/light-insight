c3a3f8644f36725210a6be4f41bdfa4bd857a2eb
dangphmqng233 <dangphmqng.233@gmail.com>
2026-03-12 15:14:43 +0700

    add CameraHealthScoreWidget

---
+++ LightInsight/Dashboard/Camera/Client/CameraHealthScoreWidget.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LightInsight.Dashboard.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LightInsight.Dashboard.Camera.Client
+{
+	/// <summary>
+	/// Interaction logic for CameraHealthScoreWidget.xaml
+	/// </summary>
+	public partial class CameraHealthScoreWidget : UserControl, IDashboardWidget
+	{
+		public event EventHandler DeleteRequested;
+
+		public CameraHealthScoreWidget()
+		{
+			InitializeComponent();
+			DeleteButton.Visibility = Visibility.Collapsed;
+		}
+		public void SetEditMode(bool isEdit)
+		{
+			DeleteButton.Visibility = isEdit ? Visibility.Visible : Visibility.Collapsed;
+		}
+		private void DeleteWidget_Click(object sender, RoutedEventArgs e)
+		{
+			DeleteRequested?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
