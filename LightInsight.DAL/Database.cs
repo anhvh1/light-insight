3cef660ce7d85004ef603b544bf2c6b788f66096
PhiNV <phidzai007@gmail.com>
2026-03-26 16:51:47 +0700

    Commit

--- LightInsight.DAL/Database.cs
+++ LightInsight.DAL/Database.cs
@@ -19,7 +19,7 @@ namespace LightInsight.DAL
             JOIN Hardware h ON d.IDHardware = h.IDHardware
             WHERE d.DeviceType = 'Camera'";
 
-            using (SqlConnection conn = new SqlConnection("Data Source= 192.168.100.10 ;Initial Catalog=Surveillance;Persist Security Info=True;User ID=dev;Password=gosol@123;TrustServerCertificate=True;Max Pool Size=400"))
+            using (SqlConnection conn = new SqlConnection(""))
             using (SqlCommand cmd = new SqlCommand(query, conn))
             {
                 conn.Open();
