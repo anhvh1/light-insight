b5b1f3fd1fbf807a90826df5ba548549592f8364
PhiNV <phidzai007@gmail.com>
2026-03-26 09:42:45 +0700

    Chỉnh sửa layout phiên bản 2

--- LightInsight/Dashboard/Dashboard/Workspace/WorkspaceModel.cs
+++ LightInsight/Dashboard/Dashboard/Workspace/WorkspaceModel.cs
@@ -1,11 +1,9 @@
-﻿using MahApps.Metro.IconPacks;
+using MahApps.Metro.IconPacks;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Text.Json.Serialization;
 
 namespace LightInsight.Dashboard.Dashboard.Workspace
 {
@@ -17,47 +15,65 @@ namespace LightInsight.Dashboard.Dashboard.Workspace
         public string Name
         {
             get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
+            set { _name = value; OnPropertyChanged(nameof(Name)); OnPropertyChanged(nameof(DisplayTitle)); }
         }
 
         private PackIconMaterialKind _icon;
         public PackIconMaterialKind Icon
         {
             get => _icon;
-            set
-            {
-                _icon = value;
-                OnPropertyChanged(nameof(Icon));
-            }
+            set { _icon = value; OnPropertyChanged(nameof(Icon)); }
         }
 
         public string Type { get; set; }
-
         public string ParentId { get; set; }
+        public string Path { get; set; }
 
         private bool _isSelected;
         public bool IsSelected
         {
             get => _isSelected;
-            set
+            set { _isSelected = value; OnPropertyChanged(nameof(IsSelected)); }
+        }
+
+        [JsonIgnore]
+        public bool IsGroup { get; set; }
+
+        private bool _isExpanded;
+        [JsonIgnore]
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set { _isExpanded = value; OnPropertyChanged(nameof(IsExpanded)); }
+        }
+
+        [JsonIgnore]
+        public string DisplayTitle
+        {
+            get
             {
-                _isSelected = value;
-                OnPropertyChanged(nameof(IsSelected));
+                if (string.IsNullOrEmpty(Name)) return string.Empty;
+                if (Type == null)
+                {
+                    var localized = Application.Current.TryFindResource(Name) as string;
+                    return localized ?? Name;
+                }
+                return Name;
             }
         }
 
         public ObservableCollection<WorkspaceModel> Children { get; set; } = new ObservableCollection<WorkspaceModel>();
 
-
         public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-        protected void OnPropertyChanged(string name)
+        public void RefreshLocalization()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            OnPropertyChanged(nameof(DisplayTitle));
+            if (Children != null)
+            {
+                foreach (var child in Children) child.RefreshLocalization();
+            }
         }
     }
 }
